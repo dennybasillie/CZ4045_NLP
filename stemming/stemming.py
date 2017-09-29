08f2724c8d166a657177af981b7c14cff60b8590
dennybasillie <denny077@hotmail.com>
2017-09-29 21:12:36 +0800

    Fix stemmer to use tokenizer

--- stemming/stemming.py
+++ stemming/stemming.py
@@ -1,29 +1,34 @@
 import os
 import operator
+import re
 from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
+from nltk.tokenize import *
 
 fileDir = os.path.dirname(os.path.realpath('_file_'))
 dataLocation = os.path.join(fileDir, 'posts.txt')
 
+
 def main():
     origFileHandler = open(dataLocation, "r")
-    origWordCount = countWords(origFileHandler)
+    origTokensList = word_tokenize(origFileHandler.read())
+    origWordCountDict = countWords(origTokensList)
+
     print "Top 20 Original:\n_______________________________"
-    origTopTwentyList = getTopTwenty(origWordCount)
+    origTopTwentyList = getTopTwenty(origWordCountDict)
     for tuple in origTopTwentyList:
         print tuple
     print("\n")
 
-    origFileHandler.seek(0)
     stemmedDataLocation = os.path.splitext(dataLocation)[0] + "_stem.txt"
     stemFileHandler = open(stemmedDataLocation, 'w+')
+    stemDerivativesDict = getStems(origTokensList, stemFileHandler)
 
-    stemDerivativesDict = getStems(origFileHandler, stemFileHandler)
     stemFileHandler.seek(0)
-    stemWordCount = countWords(stemFileHandler)
+    stemTokensList = word_tokenize(stemFileHandler.read())
+    stemWordCountDict = countWords(stemTokensList)
     print "Top 20 Stemmed:\n_______________________________"
-    stemTopTwentyList = getTopTwenty(stemWordCount)
+    stemTopTwentyList = getTopTwenty(stemWordCountDict)
     for tuple in stemTopTwentyList:
         print tuple
     print("\n")
@@ -39,32 +44,28 @@ def getOriginalsFromStems(stemDerivativesDict, stemTopTwentyList):
         topStem = topStemTuple[0]
         print topStem + " : " + " ".join(stemDerivativesDict[topStem])
 
-def getStems(inputFileHandler, outputFileHandler):
+def getStems(wordList, outputFileHandler):
     stemmer = PorterStemmer()
     stemDerivativesDict = {}
 
-    stemmedDataLocation = os.path.splitext(dataLocation)[0] + "_stem.txt"
-    stemFileHandler = open(stemmedDataLocation, 'w')
-
-    for line in inputFileHandler:
-        for word in line.split():
-            wordLower = word.lower()
-            stemmedWord = stemmer.stem(wordLower)
-            outputFileHandler.write(stemmedWord + " ")
-            if stemmedWord not in stemDerivativesDict:
-                stemDerivativesDict[stemmedWord] = []
+    for word in wordList:
+        wordLower = word.lower()
+        stemmedWord = stemmer.stem(wordLower)
+        outputFileHandler.write(stemmedWord + " ")
+        if stemmedWord not in stemDerivativesDict:
+            stemDerivativesDict[stemmedWord] = []
+            stemDerivativesDict[stemmedWord].append(wordLower)
+        else:
+            if wordLower not in stemDerivativesDict[stemmedWord]:
                 stemDerivativesDict[stemmedWord].append(wordLower)
-            else:
-                if wordLower not in stemDerivativesDict[stemmedWord]:
-                    stemDerivativesDict[stemmedWord].append(wordLower)
-        outputFileHandler.write("\n")
     return stemDerivativesDict
 
-def countWords(fileHandler):
+def countWords(wordList):
     wordCount = {}
     stops = stopwords.words('english')
-    for line in fileHandler:
-        for word in line.split():
+    for word in wordList:
+        #Do not include anything that does not contain a word character
+        if not re.match('^[\W]*$', word):
             wordLower = word.lower()
             if wordLower not in stops:
                 if wordLower not in wordCount:
