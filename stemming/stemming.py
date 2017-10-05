cf49639f050b9fc30c73914b7950b5923a9810a2
dennybasillie <denny077@hotmail.com>
2017-10-05 21:30:41 +0800

    Added stem results and stopwords

--- stemming/stemming.py
+++ stemming/stemming.py
@@ -6,62 +6,91 @@ from nltk.stem.porter import PorterStemmer
 from nltk.tokenize import *
 
 fileDir = os.path.dirname(os.path.realpath('_file_'))
-dataLocation = os.path.join(fileDir, 'posts.txt')
-
+questionLocation = os.path.join(fileDir, "..", "preprocessing", "questions-cleaned.txt")
+answerLocation = os.path.join(fileDir, "..", "preprocessing", "answers-cleaned.txt")
+stopwordsLocation = os.path.join(fileDir, "stopwords.txt")
 
 def main():
-    origFileHandler = open(dataLocation, "r")
-    origTokensList = word_tokenize(origFileHandler.read())
-    origWordCountDict = countWords(origTokensList)
+    #Parse questions and answers into single combined string
+    strPosts = ' '.join(combineQuestionsAndAnswers())
 
+    #Parse stopwords.txt into a list of stopwords
+    stopwordList = []
+    with open(stopwordsLocation, 'r') as stopwordsFileHandler:
+        stopwordList = stopwordsFileHandler.readlines()
+    stopwordList = [stopword.strip() for stopword in stopwordList]
+
+    #Tokenize the string of questions and answers, and count occurences of non-stopwords
+    origTokensList = word_tokenize(strPosts)
+    origWordCountDict = countWords(origTokensList, stopwordList)
+
+    #Print out top 20 frequent words and their occurence counts
     print "Top 20 Original:"
     origTopTwentyList = getTopTwenty(origWordCountDict)
     for tuple in origTopTwentyList:
         print tuple
     print("\n")
 
-    stemmedDataLocation = os.path.splitext(dataLocation)[0] + "_stem.txt"
-    stemFileHandler = open(stemmedDataLocation, 'w+')
-    stemDerivativesDict = getStems(origTokensList, stemFileHandler)
+    #Stem the questions and answers, and output as a single file
+    stemTokensList, stemOriginalsDict = getStems(origTokensList)
 
-    stemFileHandler.seek(0)
-    stemTokensList = word_tokenize(stemFileHandler.read())
-    stemWordCountDict = countWords(stemTokensList)
+    #Create a list of stemmed stopwords
+    stemStopwordsList, stemStopwordsOriginalsDict = getStems(stopwordList)
+
+    #Print out top 20 frequent stemmed words, their occurence counts, and their original words
+    stemWordCountDict = countWords(stemTokensList, stemStopwordsList)
     print "Top 20 Stemmed:"
     stemTopTwentyList = getTopTwenty(stemWordCountDict)
     for tuple in stemTopTwentyList:
         print tuple,
         stem = tuple[0]
-        print "=> " + " ".join(stemDerivativesDict[stem])
+        print "=> " + " ".join(stemOriginalsDict[stem])
     print("\n")
 
-    origFileHandler.close()
-    stemFileHandler.close()
+def combineQuestionsAndAnswers():
+    posts = []
+    with open(questionLocation, 'r') as questionFileHandler, open(answerLocation, 'r') as answerFileHandler:
+        for line in questionFileHandler:
+            temp = eval(line)
+            sentences = sent_tokenize(temp['question'].encode('ascii', 'ignore'))
 
-def getStems(wordList, outputFileHandler):
+            # Remove sentences with awkward newline characters (because of code section removal)
+            posts += [sentence for sentence in sentences if not '\n' in sentence]
+
+        for line in answerFileHandler:
+            temp = eval(line)
+            sentences = sent_tokenize(temp['answer'].encode('ascii', 'ignore'))
+
+            # Remove sentences with awkward newline characters (because of code section removal)
+            posts += [sentence for sentence in sentences if not '\n' in sentence]
+
+    return posts
+
+def getStems(wordList):
     stemmer = PorterStemmer()
     stemDerivativesDict = {}
+    stemWordsList = []
 
     for word in wordList:
         wordLower = word.lower()
-        stemmedWord = stemmer.stem(wordLower)
-        outputFileHandler.write(stemmedWord + " ")
+        stemmedWord = stemmer.stem(wordLower).encode('ascii', 'ignore')
+        stemWordsList.append(stemmedWord)
         if stemmedWord not in stemDerivativesDict:
             stemDerivativesDict[stemmedWord] = []
             stemDerivativesDict[stemmedWord].append(wordLower)
         else:
             if wordLower not in stemDerivativesDict[stemmedWord]:
                 stemDerivativesDict[stemmedWord].append(wordLower)
-    return stemDerivativesDict
+    return stemWordsList, stemDerivativesDict
 
-def countWords(wordList):
+def countWords(wordList, stopwordList):
     wordCount = {}
-    stops = stopwords.words('english')
+    nltkStopwords = stopwords.words('english')
     for word in wordList:
-        #Do not include anything that does not contain a word character
+        #Do not include anything that only contains symbols (non-word characters)
         if not re.match('^[\W]*$', word):
             wordLower = word.lower()
-            if wordLower not in stops:
+            if (wordLower not in nltkStopwords) and (wordLower not in stopwordList):
                 if wordLower not in wordCount:
                     wordCount[wordLower] = 1
                 else:
