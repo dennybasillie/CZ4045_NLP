185cec4c5b2162743bfa82a24670a362409ce72c
dennybasillie <denny077@hotmail.com>
2017-10-11 23:16:31 +0800

    Annotate delimited by spaces and symbols

--- annotation-util/annotate.py
+++ annotation-util/annotate.py
@@ -11,14 +11,15 @@ def main():
     annFileLocation = fileName + ".ann"
     # outputLocation = os.path.splitext(fileLocation)[0] + ".ann"
     with open(txtFileLocation, 'r') as txtFileHandler, open(annFileLocation, 'r+') as annFileHandler:
-        start, end, token = [], [], []
+        start, end, token , startSorted, endSorted, tokenSorted = [], [], [], [], [], []
         for line in annFileHandler:
             print line
             m = re.search('(T\d+)\s(\w+)\s(\d+)\s(\d+)\s(.+)', line) #token_id, token_name, start, end, token
             start.append(int(m.group(3)))
             end.append(int(m.group(4)))
             token.append(m.group(5))
-        startSorted, endSorted, tokenSorted = (list(x) for x in zip(*sorted(zip(start, end, token))))
+        if start:
+            startSorted, endSorted, tokenSorted = (list(x) for x in zip(*sorted(zip(start, end, token))))
         annFileHandler.seek(0)
         annFileHandler.truncate()
 
@@ -39,43 +40,59 @@ def main():
                         tokenId += 1
                         listIndex += 1
                     else:
-                        nextSpaceIndex = -1
+                        nextNonwordIndex = -1
                         for i,n in enumerate(line[lineIndex:]):
-                            if n.isspace():
-                                nextSpaceIndex = i + lineIndex
+                            if not n.isalnum():
+                                nextNonwordIndex = i + lineIndex
                                 break
-                        if (nextSpaceIndex <= -1):
-                            nextSpaceIndex = lineLength
-                        if startSorted[listIndex] <= processedLength + nextSpaceIndex:
+                        if (nextNonwordIndex <= -1):
+                            nextNonwordIndex = lineLength
+                        if startSorted[listIndex] <= processedLength + nextNonwordIndex:
                             word=line[lineIndex:startSorted[listIndex]-processedLength]
                             annFileHandler.write("T" + str(tokenId) + "\t" + tokenName + " " + str(processedLength + lineIndex) + " " + str(
                                 startSorted[listIndex]) + "\t" + word + "\n")
                             lineIndex = startSorted[listIndex] - processedLength
                             tokenId += 1
                         else:
-                            if lineIndex == nextSpaceIndex: #word contains only whitespace
-                                lineIndex += 1
+                            if lineIndex == nextNonwordIndex: #word contains only whitespace or symbols
+                                word = line[lineIndex:nextNonwordIndex+1]
+                                if word.isspace():
+                                    lineIndex += 1
+                                else:
+                                    annFileHandler.write("T" + str(tokenId) + "\t" + tokenName + " " + str(
+                                        processedLength + lineIndex) + " " + str(
+                                        processedLength + nextNonwordIndex + 1) + "\t" + word + "\n")
+                                    lineIndex = nextNonwordIndex + 1
+                                    tokenId += 1
                             else:
-                                word = line[lineIndex:nextSpaceIndex]
+                                word = line[lineIndex:nextNonwordIndex]
                                 annFileHandler.write("T" + str(tokenId) + "\t" + tokenName + " " + str(processedLength + lineIndex) + " " + str(
-                                    processedLength + nextSpaceIndex) + "\t" + word + "\n")
-                                lineIndex = nextSpaceIndex+1
+                                    processedLength + nextNonwordIndex) + "\t" + word + "\n")
+                                lineIndex = nextNonwordIndex
                                 tokenId += 1
                 else:
-                    nextSpaceIndex = -1
+                    nextNonwordIndex = -1
                     for i, n in enumerate(line[lineIndex:]):
-                        if n.isspace():
-                            nextSpaceIndex = i + lineIndex
+                        if not n.isalnum():
+                            nextNonwordIndex = i + lineIndex
                             break
-                    if (nextSpaceIndex <= -1):
-                        nextSpaceIndex = lineLength
-                    if lineIndex == nextSpaceIndex:  # word contains only whitespace
-                        lineIndex += 1
+                    if (nextNonwordIndex <= -1):
+                        nextNonwordIndex = lineLength
+                    if lineIndex == nextNonwordIndex:  # word contains only whitespace or symbols
+                        word = line[lineIndex:nextNonwordIndex + 1]
+                        if word.isspace():
+                            lineIndex += 1
+                        else:
+                            annFileHandler.write("T" + str(tokenId) + "\t" + tokenName + " " + str(
+                                processedLength + lineIndex) + " " + str(
+                                processedLength + nextNonwordIndex + 1) + "\t" + word + "\n")
+                            lineIndex = nextNonwordIndex + 1
+                            tokenId += 1
                     else:
-                        word = line[lineIndex:nextSpaceIndex]
+                        word = line[lineIndex:nextNonwordIndex]
                         annFileHandler.write("T" + str(tokenId) + "\t" + tokenName + " " + str(processedLength + lineIndex) + " " + str(
-                            processedLength + nextSpaceIndex) + "\t" + word + "\n")
-                        lineIndex = nextSpaceIndex + 1
+                            processedLength + nextNonwordIndex) + "\t" + word + "\n")
+                        lineIndex = nextNonwordIndex
                         tokenId += 1
             processedLength += lineLength
 
