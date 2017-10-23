e42fdd8725f9ae368817e86e79e255518ae6f281
dennybasillie <denny077@hotmail.com>
2017-10-23 16:32:21 +0800

    Added some regex rules and tokenizer result

--- tokenizer/tokenizer_re.py
+++ tokenizer/tokenizer_re.py
@@ -6,10 +6,10 @@ import sys
 
 
 def main():
-    test_list = ["@User Name", "Let Go Of Me", "http://www.google.com", "name@email.com", "@myself", "/var/folder/file.sh", "Java 7", "non-generic", "I'm", "int indexOf()",
+    test_list = ["@User Name", "http://www.google.com", "name@email.com", "@myself", "/var/folder/file.sh", "Java 7", "non-generic", "I'm", "int indexOf()",
                  "int indexOf(string mystring)", "this is an (example legit sentence)", "another(legit sentence)", ":)",
-                 "java.utils.collections", "Class<E>", "Jon Snow", "CATALINA_OPTS=\"$CATALINA_OPTS -Xms512m\"",
-                 "CATALINA_OPTS=\"$CATALINA_OPTS -xx:MaxPermSize=25m\""]
+                 "java.utils.collections", "Class<E>", "Bobby Goodfield Matthew", "CATALINA_OPTS=\"$CATALINA_OPTS -Xms512m\"",
+                 "CATALINA_OPTS=\"$CATALINA_OPTS -xx:MaxPermSize=25m\"", "e.g.", ".NET", "end. A new", "end.A new", "Axis 1.4"]
 
     # for test in test_list:
     #     print nltk.tokenize.word_tokenize(test)
@@ -22,14 +22,14 @@ def main():
         tokenId = 1
         tokenName = "Token"
 
-        #multi-word tokens, http, email, argument flags, dollar variables, one-word mentions(@), directory, package name / file name, function call, template, word with - or ' in the middle, a word, single symbol
-        regex = r".+(\s+.+)+|https?://.*|\w+@\w+(\.\w+)+|^--?\w+.+[^\"]|\$[\w_]+|^@\w+|^(/\w+)+(\.\w+|/)?|\w+(\.\w+)+|\w+\(\)|[A-Z][\w_]*<[A-Z][\w_]*>|\w+([-']?\w+)*|\w+|\W"
+        #multi-word tokens, http, email, argument flags, dollar variables, one-word mentions(@), simple abbreviations (e.g., i.e.) directory, package name / file name, extensions / .NET, function call, template, word with - or ' in the middle, a word, single symbol
+        regex = r".+(\s+.+)+|https?://.*|\w+@\w+(\.\w+)+|^--?\w+.+[^\"]|\$[\w_]+|^@\w+|([A-Za-z]\.){2,}|^(/\w+)+(\.\w+|/)?|\w+(\.\w+)+|^\.\w+|\w+\(\)|[A-Z][\w_]*<[A-Z][\w_]*>|\w+([-']?\w+)*|\w+|\W"
 
         for line in txtFileHandler:
         # for line in test_list:
             if line.strip() and not re.match("^Post-(\d)+.*Question-(\d)+$", line):
                 #Get multi-words tokens and single-word tokens
-                wordList, indexList = zip(*[(m.group(0), (m.start(), m.end())) for m in re.finditer(r'@?([A-Z]+\w+\s+)+(([A-Z]+\w+)|\d+)|\S+', line)])
+                wordList, indexList = zip(*[(m.group(0), (m.start(), m.end())) for m in re.finditer(r'@?([A-Z]+\w+\s+)+(([A-Z]+\w+)|((\d+\.)*\d+))|\S+', line)])
 
                 #Process each single-word tokens
                 for (word,index) in zip(wordList, indexList):
@@ -37,7 +37,7 @@ def main():
                     if matches:
                         subwordList, subindexList = zip(*matches)
                         for (subword, subindex) in zip(subwordList, subindexList):
-                            print (
+                            annFileHandler.write (
                                 "T" + str(tokenId) + "\t" + tokenName + " " + str(
                                     processedLength + index[0] + subindex[0]) + " " + str(
                                     processedLength + index[0] + subindex[1]) + "\t" + subword + "\n")
