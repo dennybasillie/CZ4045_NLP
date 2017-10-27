77363cb15858decd1549f5f11b7158e4d4a5f685
kurnifamili <kurnifamili@gmail.com>
2017-10-27 15:45:05 +0800

    Change irregular tokens' annotation entity type to be 'Irregular'

---
+++ annotation-util/irregular.py
@@ -0,0 +1,21 @@
+import os
+import sys
+import re
+
+def main():
+    if (len(sys.argv) < 2):
+        print("To use: python annotate.py filename(without extension)")
+        return
+    fileName = sys.argv[1]
+    annFileLocation = fileName + ".ann"
+    # outputLocation = os.path.splitext(fileLocation)[0] + ".ann"
+    with open(annFileLocation, 'r+') as input, open(fileName + "-irregular.ann", 'w') as output:
+        for line in input:
+            if re.search('\W', line.split()[-1]):
+                output.write(line.replace('Token', 'Irregular'))
+            else:
+                output.write(line)
+
+
+if __name__ == '__main__':
+    main()
