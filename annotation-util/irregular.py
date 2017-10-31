854fce8fe258cf72940332ca5df4a011df6b17da
Michael Jonathan <michj@gmail.com>
2017-11-01 02:28:25 +0800

    update irregular ann file

--- annotation-util/irregular.py
+++ annotation-util/irregular.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import re
+import enchant
 
 def main():
     if (len(sys.argv) < 2):
@@ -9,9 +10,13 @@ def main():
     fileName = sys.argv[1]
     annFileLocation = fileName + ".ann"
     # outputLocation = os.path.splitext(fileLocation)[0] + ".ann"
+    dict_word = enchant.Dict("en_US")
     with open(annFileLocation, 'r+') as input, open(fileName + "-irregular.ann", 'w') as output:
         for line in input:
-            if re.search('[\W_]', line.split()[-1]):
+            split_line = line.split()
+            token = ' '.join(split_line[4:])
+            #if re.search('[\W_]', line.split()[-1]):
+            if re.search('[\W_]', token) or not dict_word.check(token):
                 output.write(line.replace('Token', 'Irregular'))
             else:
                 output.write(line)
